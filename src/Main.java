0bb23bfcd5cfecb400881629b7e65c633fa58c1e
Grisha <guruwolf8@gmail.com>
2023-06-06 17:10:28 +0300

    Задача 4.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -54,5 +55,13 @@ public class Main {
         }
         mediumSpending = spendingMonth / 30;
         System.out.println("Средняя сумма трат за месяц составила - " + mediumSpending + " рублей.");
+    }
+    public static void task4(){
+        System.out.println("Задача 4.");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+
+        }
     }
 }
