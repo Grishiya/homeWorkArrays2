8b7c725e7773de01a3b3351c41d85fbb9faf0b81
Grisha <guruwolf8@gmail.com>
2023-06-06 15:24:48 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1.");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
